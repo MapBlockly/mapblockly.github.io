1480ae1c31111b13f6fec60c6c6d61717c038e3b
Vinod Pahuja <vinod.pahuja@gmail.com>
2024-02-16 21:07:44 +0530

    Initial import

---
+++ js/eg.js
@@ -0,0 +1,14 @@
+
+WS_STATES = {
+
+	'intro' : '{"blocks":{"languageVersion":0,"blocks":[{"type":"map","id":"@|r1W$nC(=[To+#vppo`","x":65,"y":75}]}}',
+
+	'osm' : '{"blocks":{"languageVersion":0,"blocks":[{"type":"map","id":"@|r1W$nC(=[To+#vppo`","x":65,"y":75,"inputs":{"source":{"block":{"type":"source","id":"^ASD%Cy}.kv*kRKYZblC","fields":{"name":"osm"},"inputs":{"value":{"block":{"type":"sourceOSM","id":"v_p|l3Q{3uIR}gCd6cq~"}}}}},"layer":{"block":{"type":"layer","id":"vRz9+UmLRlIYn_-qf|#X","fields":{"sourceType":"raster","source":"osm"}}}}}]}}',
+
+	'esri_sat' : '{"blocks":{"languageVersion":0,"blocks":[{"type":"map","id":"@|r1W$nC(=[To+#vppo`","x":65,"y":75,"inputs":{"source":{"block":{"type":"source","id":"^ASD%Cy}.kv*kRKYZblC","fields":{"name":"esri_sat"},"inputs":{"value":{"block":{"type":"sourceESRISat","id":"hEkOqE*(q0`j`J/9@o/w"}}}}},"layer":{"block":{"type":"layer","id":"vRz9+UmLRlIYn_-qf|#X","fields":{"sourceType":"raster","source":"esri_sat"}}}}}]}}',
+
+	'esri_vector' : '{"blocks":{"languageVersion":0,"blocks":[{"type":"map","id":"@|r1W$nC(=[To+#vppo`","x":37,"y":75,"inputs":{"style":{"block":{"type":"style","id":"6kPUo~MN$c{wPFbY%nd(","fields":{"url":""},"inputs":{"ref":{"block":{"type":"glyphESRI","id":",7JgqnP*gyG~#AF@L~{K"}},"source":{"block":{"type":"source","id":"0OHF)9vmWTM,QIHp|ln`","fields":{"name":"esri"},"inputs":{"value":{"block":{"type":"sourceESRI","id":"NgfqI@aL7xQN`~*6A-1q"}}}}},"layer":{"block":{"type":"layerESRIVector","id":"QAyg?0zwDr](paF44-.|"}}}}}}}]}}',
+	'esri_vector_nofill' : '{"blocks":{"languageVersion":0,"blocks":[{"type":"map","id":"@|r1W$nC(=[To+#vppo`","x":0,"y":54,"inputs":{"style":{"block":{"type":"style","id":"6kPUo~MN$c{wPFbY%nd(","fields":{"url":""},"inputs":{"ref":{"block":{"type":"glyphESRI","id":",7JgqnP*gyG~#AF@L~{K"}},"source":{"block":{"type":"source","id":"0OHF)9vmWTM,QIHp|ln`","fields":{"name":"esri"},"inputs":{"value":{"block":{"type":"sourceESRI","id":"NgfqI@aL7xQN`~*6A-1q"}}}}},"layer":{"block":{"type":"layerESRIHybrid","id":"|W?qi6k]J~Qyx_e#=8(7"}}}}}}}]}}',
+
+	'esri_hybrid' : '{"blocks":{"languageVersion":0,"blocks":[{"type":"map","id":"@|r1W$nC(=[To+#vppo`","x":0,"y":0,"inputs":{"style":{"block":{"type":"style","id":"6kPUo~MN$c{wPFbY%nd(","fields":{"url":""},"inputs":{"ref":{"block":{"type":"glyphESRI","id":",7JgqnP*gyG~#AF@L~{K"}},"source":{"block":{"type":"source","id":"}!(=w[pbo#hxCX7LBkUt","fields":{"name":"esri_sat"},"inputs":{"value":{"block":{"type":"sourceESRISat","id":"-)|FG;an4CJy2TzD4:(d"}}}}},"layer":{"block":{"type":"layer","id":"b*uJ9.pTDVz%/+`vFIh|","fields":{"sourceType":"raster","source":"esri_sat"}}}}}},"source":{"block":{"type":"source","id":"0OHF)9vmWTM,QIHp|ln`","fields":{"name":"esri"},"inputs":{"value":{"block":{"type":"sourceESRI","id":"NgfqI@aL7xQN`~*6A-1q"}}}}},"layer":{"block":{"type":"layerESRIHybrid","id":"|W?qi6k]J~Qyx_e#=8(7"}}}}]}}'
+};
